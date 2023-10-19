11e13c5038997e65be1d9bd3320cab327694acef
Ina Tang <inatang75@outlook.com>
2023-10-18 20:04:10 -0400

    Initial Commit

---
+++ Propedium/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Propedium
+//
+//  Created by 唐依纳 on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Propedium/PropediumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropediumApp.swift
+//  Propedium
+//
+//  Created by 唐依纳 on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct PropediumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
